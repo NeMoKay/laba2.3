07c609738d3eea1662110d1d4286257a50b9a278
NeMoKay <kay@Makkay.local>
2026-06-16 15:16:28 +0300

    все

--- ui_ui/main.cpp
+++ ui_ui/main.cpp
@@ -1,28 +1,34 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QFile>
 #include <QTranslator>
 #include <QLocale>
 #include <QStyleFactory>
+#include <QFile>
 #include <QDebug>
 
 int main(int argc, char *argv[]){
     QApplication a(argc, argv);
+
     a.setStyle(QStyleFactory::create("Fusion"));
 
     QTranslator translator;
-    if (translator.load(":/i18n/" + QLocale::system().name() + ".qm")) {
+    QString locale = QLocale::system().name();
+    QString path = ":/i18n/" + locale + ".qm";
+
+    if (translator.load(path)) {
         a.installTranslator(&translator);
     }
 
     QFile styleFile(":/style.qss");
     if (styleFile.open(QFile::ReadOnly)) {
         a.setStyleSheet(styleFile.readAll());
-    } else {
-        qDebug() << "Ошибка: файл style.qss не найден в ресурсах!";
+    }
+    else{
+        qDebug() << "style.qss не найден";
     }
 
     MainWindow w;
     w.show();
+
     return a.exec();
 }
